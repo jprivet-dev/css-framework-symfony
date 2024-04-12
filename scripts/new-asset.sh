5bb8a4941ad7ac6108dd6b97c5ca1143132021cb
jprivet-dev <jean.philippe.rivet@gmail.com>
2024-04-12 11:05:06 +0200

    feat(scripts): create new-asset.sh & new-encore.sh

---
+++ scripts/new-asset.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Usage:
+# $ . scripts/new-asset.sh "apps/foundation/easy" "PICO ✨"
+
+path=$1
+name=$2
+
+echo "Project basic install (AssetMapper)"
+echo "==================================="
+
+symfony new "${path}" --no-git
+cp scripts/Makefile.dist "${path}/Makefile"
+sed -i "s/__NAME__/${name}/g" "${path}/Makefile"
+
+cd "${path}"
+composer require --dev symfony/maker-bundle
+composer require --dev symfony/profiler-pack
+
+composer require symfony/asset-mapper symfony/asset symfony/twig-pack
+
+make
---
+++ scripts/new-encore.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Usage:
+# $ . scripts/new-encore.sh "apps/foundation/easy" "FOUNDATION ❄️"
+
+path=$1
+name=$2
+
+echo "Project basic install (Webpack Encore)"
+echo "======================================"
+
+symfony new "${path}" --no-git
+cp scripts/Makefile.dist "${path}/Makefile"
+sed -i "s/__NAME__/${name}/g" "${path}/Makefile"
+
+cd "${path}"
+composer require --dev symfony/maker-bundle
+composer require --dev symfony/profiler-pack
+
+composer require symfony/webpack-encore-bundle
+npm install
+
+make
